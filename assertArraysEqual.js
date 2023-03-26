e888df25cd7ba6773900901440737704ae88f589
AliyaJK <kanani.aliya@gmail.com>
2023-03-26 21:56:12 +0000

    A function to compare to arrays and print success or fail message

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+} else {
+    let result = false;
+
+    for (let i = 0; i < arr1.length; i++) {
+
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        } else {
+            result = true;
+        }
+    }
+    return result;
+}
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 4], [1, 2, 3])
+
+
