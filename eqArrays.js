a3c058818cf9c2028cd45c4a5933ecf99268161b
AliyaJK <kanani.aliya@gmail.com>
2023-03-26 01:29:05 +0000

    a function to check if arrays are equal to each other

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+// function that takes in two arrays
+// compares them to each other
+// returns true for perfect match, false if not
+// use assertEqual to write test cases
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+} else {
+    let result = false;
+
+    // comparing each element of array 
+    for (let i = 0; i < arr1.length; i++) {
+
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        } else {
+            result = true;
+        }
+    }
+    return result;
+}
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
