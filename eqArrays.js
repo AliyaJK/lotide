a48eb4bb61b9eb7019f8686f2039bd0f801aaf15
AliyaJK <kanani.aliya@gmail.com>
2023-03-30 22:51:47 +0000

    after eslint

--- eqArrays.js
+++ eqArrays.js
@@ -12,23 +12,23 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
-} else {
+  } else {
     let result = false;
 
-    // comparing each element of array 
+    // comparing each element of array
     for (let i = 0; i < arr1.length; i++) {
 
-        if (arr1[i] !== arr2[i]) {
-            return false;
-        } else {
-            result = true;
-        }
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } else {
+        result = true;
+      }
     }
     return result;
-}
-}
+  }
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
