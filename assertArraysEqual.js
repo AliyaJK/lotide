eae5507b11a517e2ed7e374a53ee0af01a945954
AliyaJK <kanani.aliya@gmail.com>
2023-03-30 22:52:19 +0000

    after eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,24 +7,24 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
-} else {
+  } else {
     let result = false;
 
     for (let i = 0; i < arr1.length; i++) {
 
-        if (arr1[i] !== arr2[i]) {
-            return false;
-        } else {
-            result = true;
-        }
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } else {
+        result = true;
+      }
     }
     return result;
-}
-}
+  }
+};
 
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 2, 4], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 4], [1, 2, 3]);
 
 
