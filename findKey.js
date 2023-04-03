e1ac9d1aa3b4897606045fa9ba83477e4ede7da2
AliyaJK <kanani.aliya@gmail.com>
2023-04-03 01:23:44 +0000

    a function that takes in an object and a callback and returns the key for which the callback returns a truthy value, or undefined

---
+++ findKey.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKey function which takes in an object and a callback
+// should scan the object and return the first key for which the callback returns a truthy value
+// if no key is found it should return undefined
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined
+}
+
+const results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) 
+
+assertEqual(results1, "noma")
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKey(bestTVShowsByGenre, x => x === "The Expanse"), "sciFi");
+assertEqual(findKey(bestTVShowsByGenre, x => x === "That '70s Show"), undefined);
