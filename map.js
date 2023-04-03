b31c2f29835695bd0d109abb1ad059cd1c5f0b66
AliyaJK <kanani.aliya@gmail.com>
2023-04-03 00:38:40 +0000

    my own map function - updated after eslint

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ const map = function(array, callback) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
@@ -42,12 +42,12 @@ const eqArrays = function(arr1, arr2) {
 };
 
 // test cases
-const numbers = [1, 5, 6, 12]
+const numbers = [1, 5, 6, 12];
 const results2 = map(numbers, number => number * 5);
-assertArraysEqual(results2, [5, 25, 30, 60])
+assertArraysEqual(results2, [5, 25, 30, 60]);
 
-const results3 = map(words, word => word.length)
-assertArraysEqual(results3, [6, 7, 2, 5, 3])
+const results3 = map(words, word => word.length);
+assertArraysEqual(results3, [6, 7, 2, 5, 3]);
 
-const results4 = map(words, word => word + "s")
-assertArraysEqual(results4, ["grounds", "controls", "tos", "majors", "toms"])
+const results4 = map(words, word => word + "s");
+assertArraysEqual(results4, ["grounds", "controls", "tos", "majors", "toms"]);
