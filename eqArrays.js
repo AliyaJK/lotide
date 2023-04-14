140e77621bd88fdf050100d769d151b827ad57c7
AliyaJK <kanani.aliya@gmail.com>
2023-04-14 17:31:14 +0000

    refactoring lotide functions

--- eqArrays.js
+++ eqArrays.js
@@ -3,14 +3,6 @@
 // returns true for perfect match, false if not
 // use assertEqual to write test cases
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -27,7 +19,5 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), false); // => should PASS
-
+module.exports = eqArrays;
 
